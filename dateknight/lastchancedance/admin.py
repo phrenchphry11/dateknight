ac40f37f54a1612023c543ae316e646988437bc9
Holly French <holly.french@prezi.com>
2013-05-10 23:45:59 +0200

    admin page

---
+++ dateknight/lastchancedance/admin.py
@@ -0,0 +1,6 @@
+from dates.models import Student, Crush, Match
+from django.contrib import admin
+
+admin.site.register(Student)
+admin.site.register(Crush)
+admin.site.register(Match)
--- dateknight/lastchancedance/views.py
+++ dateknight/lastchancedance/views.py
@@ -5,5 +5,10 @@ from django.http import HttpResponse, HttpResponseRedirect
 def index(request):
     return HttpResponse("Hello.  Let us help you find luv here.")
 
+@login_required
 def confirm(request):
-	return HttpResponseRedirect("/confirm/")
+    return HttpResponseRedirect("/confirm/")
+
+@login_required
+def dashboard(request):
+    pass
